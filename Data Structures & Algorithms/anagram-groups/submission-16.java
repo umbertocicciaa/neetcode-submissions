b3bfdc83129317c48e79ef30f3e76231a6032932
Umberto Domenico Ciccia <umbertociccia@icloud.com>
2026-07-05 15:59:49 +0100

    Add: anagram-groups - submission-16

---
+++ Data Structures & Algorithms/anagram-groups/submission-16.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null) {
+            return new ArrayList<>();
+        }
+        final Map<String, List<String>> res = new HashMap<>();
+        for (final String s : strs) {
+            final int[] count = new int[26];
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+            final String key = Arrays.toString(count);
+            res.putIfAbsent(key, new ArrayList<>());
+            res.get(key).add(s);
+        }
+        return new ArrayList<>(res.values());
+    }
+}
