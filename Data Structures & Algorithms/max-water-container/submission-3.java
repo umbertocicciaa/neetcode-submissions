6a7ba6a4ffc44b811f56b06065011c44a7254f1b
Umberto Domenico Ciccia <umbertociccia@icloud.com>
2026-07-08 20:55:16 +0100

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxArea(int[] heights) {
+        int left = 0;
+        int right = heights.length - 1;
+        int res = 0;
+        while (left <= right) {
+            final int min = Math.min(heights[left], heights[right]);
+            res = Math.max(min * (right - left), res);
+
+            if (heights[left] < heights[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return res;
+    }
+}
