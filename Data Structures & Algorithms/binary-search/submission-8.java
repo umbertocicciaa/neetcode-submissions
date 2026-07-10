aefa637e0fae5ce12b1b509d851f3ba72a8166e8
Umberto Domenico Ciccia <umbertociccia@icloud.com>
2026-07-10 20:14:12 +0100

    Add: binary-search - submission-8

---
+++ Data Structures & Algorithms/binary-search/submission-8.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if (nums == null) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            final int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
