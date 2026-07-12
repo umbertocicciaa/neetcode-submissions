2f278e6603b36925a4413bc0847b86ad1c4c94af
Umberto Domenico Ciccia <umbertociccia@icloud.com>
2026-07-12 11:29:10 +0100

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int l = 0;
+        int r = 1;
+        int result = 0;
+        for (; r < prices.length; r++) {
+            if (prices[l] >= prices[r]) {
+                l = r;
+            } else {
+                final int profit = prices[r] - prices[l];
+                result = Math.max(profit, result);
+            }
+        }
+        return result;
+    }
+}
