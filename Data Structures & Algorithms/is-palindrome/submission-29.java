1edc9acaf3303b5825e03c1413af1597a82f2b2d
Umberto Domenico Ciccia <umbertociccia@icloud.com>
2026-07-12 12:00:37 +0100

    Add: is-palindrome - submission-29

---
+++ Data Structures & Algorithms/is-palindrome/submission-29.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !alphaNum(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !alphaNum(s.charAt(right))) {
+                right--;
+            }
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static boolean alphaNum(final char c) {
+        return (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' || c >= '0' && c <= '9');
+    }
+}
