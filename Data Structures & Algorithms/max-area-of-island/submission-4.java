b6d9bef20ea263dd2778759316c122ff932333f2
Umberto Domenico Ciccia <umbertociccia@icloud.com>
2026-08-30 15:42:29 +0100

    Add: max-area-of-island - submission-4

---
+++ Data Structures & Algorithms/max-area-of-island/submission-4.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                maxArea = Math.max(maxArea, area(grid, i, j));
+            }
+        }
+        return maxArea;
+    }
+
+    private int area(int[][] grid, int r, int c) {
+        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] == 0) {
+            return 0;
+        }
+        grid[r][c] = 0;
+        int area = 1;
+        area += area(grid, r + 1, c);
+        area += area(grid, r, c + 1);
+        area += area(grid, r - 1, c);
+        area += area(grid, r, c - 1);
+        return area;
+    }
+}
