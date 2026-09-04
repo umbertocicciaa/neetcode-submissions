7df7690fd26139908d1f3add3e3646480f5e3c8a
Umberto Domenico Ciccia <umbertociccia@icloud.com>
2026-09-04 12:09:14 +0100

    Add: path-sum - submission-0

---
+++ Data Structures & Algorithms/path-sum/submission-0.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return hasPath(root, targetSum, 0);
+    }
+
+    private static boolean hasPath(TreeNode root, int targetSum, int subSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null && subSum + root.val == targetSum) {
+            return true;
+        }
+        if (hasPath(root.left, targetSum, subSum + root.val)) {
+            return true;
+        }
+        if (hasPath(root.right, targetSum, subSum + root.val)) {
+            return true;
+        }
+        subSum -= root.val;
+        return false;
+    }
+}
