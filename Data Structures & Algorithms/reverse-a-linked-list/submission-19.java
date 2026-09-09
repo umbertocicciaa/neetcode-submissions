52a1809c89c1f08698c2b901de20a76571464973
Umberto Domenico Ciccia <umbertociccia@icloud.com>
2026-09-09 20:10:35 +0100

    Add: reverse-a-linked-list - submission-19

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-19.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        return reverse(head, null);
+    }
+
+    private static ListNode reverse(ListNode node, ListNode prev) {
+        if (node == null) {
+            return prev;
+        }
+        var temp = node.next;
+        node.next = prev;
+        return reverse(temp, node);
+    }
+}
