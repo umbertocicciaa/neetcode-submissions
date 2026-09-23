ea472b9959e7ddd236a59086286a945a2499e52b
Umberto Domenico Ciccia <umbertociccia@icloud.com>
2026-09-23 21:10:08 +0100

    Add: minimum-stack - submission-19

---
+++ Data Structures & Algorithms/minimum-stack/submission-19.java
@@ -0,0 +1,31 @@
+class MinStack {
+    final Stack<Integer> minStack;
+    final Stack<Integer> stack;
+
+    public MinStack() {
+        minStack = new Stack<>();
+        stack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        int val = stack.pop();
+        if (val == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
